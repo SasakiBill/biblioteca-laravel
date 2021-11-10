e91fb0ed8f716d3568877936beafa9372b76b0cb
João Pedro Kelniar <67113082+SasakiBill@users.noreply.github.com>
2021-11-10 11:44:50 -0300

    commit #9

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,9 +22,9 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'phone',
         'password',
         'confirmpassword',
+        'phone',
         'is_admin',
     ];
 
