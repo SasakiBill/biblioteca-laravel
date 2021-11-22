a02d6e3465d01d1939d699dd03920ec483f1f110
João Pedro Kelniar <terabill41@gmail.com>
2021-11-22 14:53:10 -0300

    tentativa de upp registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,16 +8,17 @@
     <div class="formulario">
         <form method="POST" action="{{ route('register') }}">
             @csrf
-
-            <label for="name" class="form-label"></label>
+            <div class="namediv-container">
+                <label for="name" class="form-label"></label>
                 <input id="name" name="name" class="form-control @error('name') is-invalid @enderror" type="text" placeholder="Digite seu Nome" value="{{ old('name') }}">
                 @error('name')
                     <span class="invalid-feedback" role="alert">
                         {{ $message }}
                     </span>
                 @enderror
-                <br>
-                <br>
+            </div>
+            
+
             <label for="email" class="form-label"></label>
                 <input id="email" name="email" class="form-control @error('email') is-invalid @enderror" type="email" placeholder="Digite seu Email" value="{{ old('email') }}">
                 @error('email')
@@ -25,8 +26,7 @@
                         {{ $message }}
                     </span>
                 @enderror
-                <br>
-                <br>
+                
             <label for="phone" class="form-label"></label>
                 <input id="phone" name="phone" class="form-control @error('phone') is-invalid @enderror" type="tel" placeholder="Digite seu Telefone" value="{{ old('telephone') }}">
                 @error('phone')
@@ -34,8 +34,7 @@
                         {{ $message }}
                     </span>
                 @enderror
-                <br>
-                <br>
+               
             <label for="password" class="form-label"></label>
                 <input id="password" name="password" class="form-control @error('password') is-invalid @enderror" type="password" placeholder="Digite sua Senha">
                 @error('password')
@@ -43,8 +42,7 @@
                         {{ $message }}
                     </span>
                 @enderror
-                <br>
-                <br>
+                
             <label for="confirmPassword" class="form-label"></label>
                 <input id="confirmPassword" name="password_confirmation" class="form-control @error('confirmPassword') is-invalid @enderror" type="password" placeholder="Confirme sua Senha" id="senhacc">
                 @error('confirmPassword')
@@ -52,8 +50,7 @@
                         {{ $message }}
                     </span>
                 @enderror
-                <br>
-                <br>
+                
 
             <input type="submit" value="Cadastrar!">
         </form>
