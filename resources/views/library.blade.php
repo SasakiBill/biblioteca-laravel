2c26b5af4d710f7077e2220df25e34340e44c8f7
João Pedro Kelniar <terabill41@gmail.com>
2021-11-23 14:58:21 -0300

    update bootstrap (topnav bugado)

--- resources/views/library.blade.php
+++ resources/views/library.blade.php
@@ -1,64 +1,36 @@
-@extends('layouts.topnavuser')
-@extends('layouts.footeruser')
 
-<section class="library-init">
-    <br><br>
-    <h1>Livros Disponíveis</h1>
-    <table border="1">
-        <tr>
-            <th>Nome do Livro</th>
-            <th>Descrição</th>
-            <th>Autor</th>
-            <th>Categoria</th>
-            <th>Disponibilidade</th>
-            <th>Quer emprestar o livro?</th>
-        </tr>
-        @foreach ($books as $item)
-            
+{{-- @extends('layouts.topnavuser') --}}
+@extends('layouts.bootstraptopnav')
+@extends('layouts.bootstrapfooter')
+{{-- @extends('layouts.footeruser') --}}
+
+
+<section class="library-init"  style="margin-top: -4vw;" >
+    <div class="container">
+        <br><br>
+        <h1>Livros Disponíveis</h1>
+        <table class="table table-dark"">
             <tr>
-                <td>{{ $item->name }} </td>
-                <td>{{ $item->description}} </td>
-                <td>{{ $item->author }} </td>
-                <td>{{ $item->category}} </td>
-                <td>{{ $item->book_status }}</td>
-                <td> <a href="{{ url('add-to-loan/'.$item->id) }}">Emprestar livro</a> </td>
+                <th>Nome do Livro</th>
+                <th>Descrição</th>
+                <th>Autor</th>
+                <th>Categoria</th>
+                <th>Disponibilidade</th>
+                <th>Quer emprestar o livro?</th>
             </tr>
-        @endforeach
-<!--
-        <div class="modalacesso-container">
-            <a href="#openModal">Abrir Cadastro</a>
-    
-            <div id="openModal" class="modalDialog">
-                <a href="#close" title="Close" class="close">X</a>
-                <div class="titulo">
-                    <p>Formulário Simples
-                    <p>
-    
-                </div>
-            
-            <div class="formulario">
-                <form method="get">
-                    <label>Name:
-                        <input type="text" placeholder="example" id="username">
-                    </label>
-                    <label>E-mail:
-                        <input type="email" placeholder="email@email" id="email">
-                    </label>
-                    <label>CEP:
-                        <input name="cep" type="text" id="cep" value="" size="10" maxlength="9"
-                            onblur="pesquisacep(this.value);" /></label><br />
-                    <label>Street:
-                        <input name="rua" type="text" id="rua" size="60" /></label><br />
-                    <label>District:
-                        <input name="bairro" type="text" id="bairro" size="40" /></label><br />
-                    <label>City:
-                        <input name="cidade" type="text" id="cidade" size="40" /></label><br />
-                    <label>State:
-                        <input name="uf" type="text" id="uf" size="2" /></label><br />
-                        <input type="submit" value="Submit">
-                    </form>
-                </div>
-            </div>-->
-        </div>
-    </table>
+            @foreach ($books as $item)
+                
+                <tr>
+                    <td>{{ $item->name }} </td>
+                    <td>{{ $item->description}} </td>
+                    <td>{{ $item->author }} </td>
+                    <td>{{ $item->category}} </td>
+                    <td>{{ $item->book_status }}</td>
+                    <td> <a href="{{ url('add-to-loan/'.$item->id) }}">Emprestar livro</a> </td>
+                </tr>
+            @endforeach
+   
+            </div>
+        </table>
+    </div>
 </section>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -7,11 +7,13 @@
 <section class="section-init position-fixed align-top" style="margin-top: 4vw;" >
     <div class="container">
         <h1>Página Inicial</h1>
-        <div class="caixainicial-container">    
-            <p>Sistema simples para visualização e empréstimos de livros.
-        </div>
-        <div class="col-md-5 col-xs-5  float-right mx-auto d-block" >
-            <img src="/img/bibseis.jpg" alt="biblogo" class="img-responsive" style="display: grid;">
+        <div class="clearfix bg-white">
+            <div class="float-start">
+                <p>Sistema simples para visualização e empréstimos de livros.</p>
+            </div>
+            <div class="float-end">
+                <img src="/img/bibseis.jpg" alt="biblogo">
+            </div>
         </div>
     </div>
 
